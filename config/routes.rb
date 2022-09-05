fa36476c8ad910fe5f593008ca0ea8d1ae5cc3b3
wtitsantetsu <santetsu1008@gmail.com>
2022-09-05 16:33:18 +0900

    progate3まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'posts/index'
+  get "posts/new" => "posts#new"
+  post "posts/create" => "posts#create"
+  get "posts/:id" =>  "posts#show"
   get "/" => "home#top"
   get "about" => "home#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

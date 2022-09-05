b82800a3bd70f9c5163fa8155c506ea38c5cfb39
wtitsantetsu <santetsu1008@gmail.com>
2022-09-05 14:48:02 +0900

    progate2まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'posts/index'
   get "/" => "home#top"
   get "about" => "home#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

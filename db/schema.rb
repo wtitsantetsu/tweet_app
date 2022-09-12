6e4c83059f382dfd3e2f11eba14ee654215c90b0
wtitsantetsu <santetsu1008@gmail.com>
2022-09-12 18:44:20 +0900

    progate11まで(password教科)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_12_055445) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_12_091205) do
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
@@ -31,7 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_12_055445) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_name"
-    t.string "password"
+    t.string "password_digest"
   end
 
 end
